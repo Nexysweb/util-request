4cdf9a8c66a5271ae41392f5a72a66439e2a04f4
John B <john@nexys.ch>
2019-09-24 00:21:49 +0200

    compile index

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import I18n from './dist/i18n';
+/*import I18n from './dist/i18n';
 import Request from './dist/request';
 
-export default { I18n, Request };
+export default { I18n, Request };*/
+
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _i18n = _interopRequireDefault(require("./dist/i18n"));
+
+var _request = _interopRequireDefault(require("./dist/request"));
+
+var _default = {
+  I18n: _i18n["default"],
+  Request: _request["default"]
+};
+exports["default"] = _default;
