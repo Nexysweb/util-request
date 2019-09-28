52a503d8bbee5479ac48b99edcad8ba8be32f374
John B <john@nexys.ch>
2019-09-28 18:35:54 +0200

    add refernc ein iundex

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 
 var _i18n = (require("./dist/i18n"));
-
 var _request = (require("./dist/request"));
+var _store = (require("./dist/store"));
 
 var _default = {
   I18n: _i18n["default"],
-  Request: _request["default"]
+  Request: _request["default"],
+  Store: _request["store"]
 };
 exports["default"] = _default;
