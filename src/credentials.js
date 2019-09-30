b6e713def43364f7443fa64b82310263d7f01faa
John B <john@nexys.ch>
2019-09-30 15:10:20 +0200

    added test

--- src/credentials.js
+++ src/credentials.js
@@ -1,16 +1,19 @@
+import Cookies from  'js-cookie';
+import Store from './store';
+
 const authKey = 'auth';
 const profileKey = 'profile';
 const permissionsKey = 'permissions';
 const localeKey = 'locale';
 
-export const storeCredentials = (token, profile, permissions, locale) => {
+export const set = (token, profile, permissions, locale) => {
   Cookies.set(authKey, { token });
   Store.set(profileKey, profile);
   Store.set(permissionsKey, permissions);
   Store.set(localeKey, locale);
 }
 
-export const removeCredentials = () => {
+export const remove = () => {
   Cookies.remove(authKey);
   Store.remove(profileKey);
   Store.remove(permissionsKey);
@@ -23,4 +26,8 @@ export const getPermissions = () => Store.get(permissionsKey);
 
 export const getLocale = () => Store.get(localeKey);
 
-export default { getProfile, getPermissions, getLocale };
+export const getToken = () => Cookies.get(authKey);
+
+const isDefined = () => getToken() !== undefined;
+
+export default { set, remove, getProfile, getPermissions, getLocale, getToken, isDefined };
---
+++ src/credentials.test.js
@@ -0,0 +1,23 @@
+import Credentials from './credentials';
+
+const profile = {name: 'my profile'}
+const token = 'my token';
+const permissions = ['a', 'b', 'c'];
+const locale = 'en';
+
+test('set credentials', () => {
+  Credentials.set(token, profile, permissions, locale);
+
+  expect(Credentials.getProfile()).toEqual(profile);
+  expect(Credentials.getPermissions()).toEqual(permissions);
+  expect(Credentials.getLocale()).toEqual(locale);
+  expect(Credentials.isDefined()).toEqual(true);
+});
+
+test('remove credentials', () => {
+  Credentials.remove(token, profile, permissions, locale);
+
+  expect(Credentials.getProfile()).toEqual(undefined);
+  expect(Credentials.getToken()).toEqual(undefined);
+  expect(Credentials.isDefined()).toEqual(false);
+});
