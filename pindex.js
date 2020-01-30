20201ef07b33d1d2a99878cf063a2fc168900475
John B <john@nexys.ch>
2020-01-30 15:45:05 +0100

    clean + add trabvis

--- pindex.js
+++
@@ -1,7 +0,0 @@
-import Credentials from './dist/credentials';
-import I18n from './dist/i18n';
-import Request from './dist/request';
-import Store from './dist/store';
-import Utils from './dist/utils';
-
-export default { Credentials, I18n, Request, Store, Utils };
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import Index from './pindex';
+import Index from './index';
 
 test('make sure all dependencies do not return null', () => {
   Object.keys(Index).map(k => {
