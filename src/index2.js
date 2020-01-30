4155a28406f9905deb8b58b911290546a1c685a5
John B <john@nexys.ch>
2020-01-30 16:03:46 +0100

    add index

---
+++ src/index2.js
@@ -0,0 +1,7 @@
+import Credentials from './credentials';
+import I18n from './i18n';
+import Request from './request';
+import Store from './store';
+import Utils from './utils';
+
+export default { Credentials, I18n, Request, Store, Utils };
